0e782e4a5a3826ab20780d4c716947fc5f1794c0
guilleto <guilleccubillos@hotmail.com>
2023-05-20 12:24:33 -0500

    Register screen init config

---
+++ src/app/Register/register.ts
@@ -0,0 +1,99 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import {
+  AbstractControl,
+  FormBuilder,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+import { Router } from '@angular/router';
+@Component({
+  selector: 'register',
+  templateUrl: './register.html',
+  styleUrls: ['./register.css'],
+})
+export class register {
+  registerForm: FormGroup;
+  constructor(
+    private http: HttpClient,
+    private router: Router,
+    private formBuilder: FormBuilder
+  ) {
+    this.registerForm = this.formBuilder.group({
+      username: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(3),
+          Validators.maxLength(20),
+        ],
+      ],
+      password: [
+        '',
+        Validators.required,
+        Validators.minLength(3),
+        Validators.maxLength(20),
+      ],
+      confirmPassword: ['', [Validators.required, this.passwordMatchValidator]],
+      email: ['', [Validators.required, Validators.email]],
+      tel: ['', Validators.required, Validators.min(3000000000)],
+      genero: ['', Validators.required],
+      lenguaje: ['', Validators.required],
+    });
+  }
+
+  // ngOnInit(): void {}
+
+  username: string = '';
+  password: string = '';
+  confirmPassword: string = '';
+  lenguaje: string = '';
+  genero: string = '';
+  tel: number | null = null;
+  email: string = '';
+
+  goToLogin() {
+    this.router.navigate(['/']);
+  }
+
+  onSubmit() {
+    if (this.registerForm.valid) {
+      // Aquí puedes realizar las acciones necesarias al enviar el formulario
+      console.log('Formulario enviado');
+      console.log(this.registerForm.value);
+      this.goToLogin();
+    } else {
+      // Marcar los campos como tocados para mostrar los mensajes de validación
+      this.markAllFieldsAsTouched();
+    }
+    // Aquí puedes agregar la lógica para enviar los datos del formulario al servidor
+  }
+
+  markAllFieldsAsTouched() {
+    Object.keys(this.registerForm.controls).forEach((field) => {
+      const control = this.registerForm.get(field);
+      control!.markAsTouched({ onlySelf: true });
+    });
+  }
+
+  passwordMatchValidator(control: AbstractControl) {
+    if (control != null) {
+      const passwordControl = control.get('password');
+      const confirmPasswordControl = control.get('confirmPassword');
+
+      if (!passwordControl || !confirmPasswordControl) {
+        return null; // No se puede realizar la validación, se asume que es correcto
+      }
+
+      const password = passwordControl.value;
+      const confirmPassword = confirmPasswordControl.value;
+
+      if (password !== confirmPassword) {
+        return { passwordMismatch: true };
+      } else {
+        return null;
+      }
+    }
+    return null;
+  }
+}
